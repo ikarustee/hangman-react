547735f051c37fff1fca47a75bd14afd66a4f65f
Tanja Karius <tanja.karius@googlemail.com>
2021-12-04 10:06:18 +0100

    Follow tutorial and add comments how it all works

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css"
-import React from "react"
+import React, {useState, useEffect} from "react"
 import Figure from "./components/Figure"
 import Header from "./components/Header"
 import WrongLetters from "./components/WrongLetters"
@@ -9,20 +9,53 @@ const wordList = ["declaration", "tractor", "library", "computer"]
 let selectedWord = wordList[Math.floor(Math.random() * wordList.length)]
 
 function App() {
-  const checkIfLetterIsPresent = (event) => {
-    if (selectedWord.split("").includes(event.target.value)) {
-      // console.log('yes')
-      // correct letter has to be added to a empty array, keep adding the item
-      //
+  // Check if game can be played
+  const [playable, setPlayable] = useState(true)
+  // Check the correct letters
+  const [correctLetters, setCorrectLetters] = useState([])
+  // Check the wrong letters
+  const [wrongLetters, setWrongLetters] = useState([])
+
+  useEffect(()=>{
+    const handleKeydown = (e) => {
+      const {key, keyCode} = e
+      if(playable && keyCode >= 65 && keyCode <= 90) {
+        const letter= key.toLowerCase()
+
+        if(selectedWord.includes(letter)) {
+          // if the current correct letters do not include the letter
+          if(!correctLetters.includes(letter)) {
+            // take the current letters, create new array from a copy, spread all current letters we have and add the new letter
+            setCorrectLetters((currentLetters) => [...currentLetters, letter])
+          } else {
+            // notification
+          }
+        } else {
+          // if the letter is not a correct letter
+          if(!wrongLetters.includes(letter)){
+            // take the current wrong letters and add the new wrong letter into the new copied array
+            setWrongLetters((wrongLetters) => [...wrongLetters, letter])
+          } else {
+            // notification
+          }
+        }
+      }
     }
-  }
+    window.addEventListener('keydown', handleKeydown)
+    return () => window.removeEventListener('keydown', handleKeydown)
+    // Empty array will call useEffect only on the initial render
+    // Alternativ: put in a dependency
+  }, [correctLetters, wrongLetters, playable])
+
   return (
-    <div className="App">
+    <div>
       <Header />
-      <Figure />
-      <WrongLetters />
-      <input onKeyUp={checkIfLetterIsPresent} />
-      <Word selectedWord={selectedWord} />
+        <div className="game-container">
+          <Figure />
+          <WrongLetters wrongLetters={wrongLetters} />
+          {/* <input onKeyUp={checkIfLetterIsPresent} /> */}
+          <Word selectedWord={selectedWord} correctLetters={correctLetters} />
+        </div>
     </div>
   )
 }
--- src/components/WrongLetters.js
+++ src/components/WrongLetters.js
@@ -1,5 +1,18 @@
 import React from "react";
 
-export default function WrongLetters() {
-  return <div>Wrong Letters</div>;
+function WrongLetters({wrongLetters}) {
+  /* Add comma between the wrong letters in the list / in the spans */
+  return (
+    <div className="wrong-letters-container">
+      <div>
+      {wrongLetters.length > 0 && <p>Wrong</p>}
+        {wrongLetters
+        .map((letter, i) => <span key={i} className="wrongletter">{letter}</span>)
+        .reduce((prevValue, currValue) => prevValue === null ? [currValue] : [prevValue, ', ', currValue], null )
+        }
+      </div>
+    </div>
+  );
 }
+
+export default WrongLetters
